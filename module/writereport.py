ee5b63c86a406d09bbb51ed2b33b14ef90e802f3
Gun8hoot <gun8hoot@proton.me>
2024-06-06 20:15:41 +0200

    Do an update to compatibility between linux and windows

--- module/writereport.py
+++ module/writereport.py
@@ -1,11 +1,22 @@
 import time, os 
 
 def writerep(given_hash, word):
-    path = "report/"
+    path_linux = "report/"
+    path_win = 'report\\'
     try:
-        os.makedirs(path)
+        if os.name == 'nt':
+            os.makedirs(path_win)
+        else:
+            os.makedirs(path_linux)
     except FileExistsError:
         pass
     
     create_report = open(path + time.strftime('%S-%M-%H|%d-%m-%Y') + "_report-crackers.txt", "w")
     write_report = create_report.writelines(f"{given_hash} : {word}")
+
+    if os.name == 'nt':
+        create_report = open(path_win + time.strftime('%S-%M-%H|%d-%m-%Y') + "_report-crackers.txt", "w")
+        write_report = create_report.writelines(f"{given_hash} : {word}")
+    else:
+        create_report = open(path_linux + time.strftime('%S-%M-%H|%d-%m-%Y') + "_report-crackers.txt", "w")
+        write_report = create_report.writelines(f"{given_hash} : {word}")
